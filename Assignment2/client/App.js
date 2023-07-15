4be843de9c8f27468889f196d297a4bb24c676f9
Shubham Singh <shubham212a@gmail.com>
2023-07-15 22:50:17 +0530

    updated all the functionalities

--- Assignment2/client/App.js
+++ Assignment2/client/App.js
@@ -1,7 +1,6 @@
 import React from 'react';
 import { NavigationContainer } from '@react-navigation/native';
 import { createStackNavigator } from '@react-navigation/stack';
-import HomeScreen from './screens/HomeScreen';
 import EmployeeListScreen from './screens/EmployeeListScreen';
 import EmployeeDetailsScreen from './screens/EmployeeDetailsScreen';
 import EmployeeFormScreen from './screens/EmployeeFormScreen';
@@ -12,7 +11,6 @@ const App = () => {
     return (
         <NavigationContainer>
             <Stack.Navigator initialRouteName="Home">
-                <Stack.Screen name="Home" component={HomeScreen} />
                 <Stack.Screen name="EmployeeList" component={EmployeeListScreen} />
                 <Stack.Screen name="EmployeeDetails" component={EmployeeDetailsScreen} />
                 <Stack.Screen name="EmployeeForm" component={EmployeeFormScreen} />
--- Assignment2/client/screens/EmployeeListScreen.js
+++ Assignment2/client/screens/EmployeeListScreen.js
@@ -1,49 +1,90 @@
 import React, { useEffect, useState } from 'react';
-import { View, FlatList, Text, TouchableOpacity, StyleSheet } from 'react-native';
+import { View, FlatList, Text, TouchableOpacity, TextInput, StyleSheet } from 'react-native';
 import { getEmployees, deleteEmployee } from '../services/api';
+import { FontAwesome } from '@expo/vector-icons';
 
 const EmployeeListScreen = ({ navigation }) => {
     const [employees, setEmployees] = useState([]);
+    const [searchQuery, setSearchQuery] = useState('');
+    const [searchResults, setSearchResults] = useState([]);
 
     useEffect(() => {
         fetchEmployees();
     }, []);
 
+    useEffect(() => {
+        if (searchQuery) {
+            const filteredEmployees = employees.filter((employee) =>
+                employee.employee_name.toLowerCase().includes(searchQuery.toLowerCase())
+            );
+            setSearchResults(filteredEmployees);
+        } else {
+            setSearchResults(employees);
+        }
+    }, [searchQuery]);
+
     const fetchEmployees = async () => {
         try {
             const data = await getEmployees();
             setEmployees(data);
+            setSearchResults(data);
         } catch (error) {
             console.error('Error fetching employees:', error);
         }
     };
 
-    const handleDeleteEmployee = async (id) => {
+    const handleDelete = async (id) => {
         try {
             await deleteEmployee(id);
-            fetchEmployees();
-            Alert.alert('Success', 'Employee deleted!');
+            const updatedEmployees = employees.filter((employee) => employee.id !== id);
+            setEmployees(updatedEmployees);
+            setSearchResults(updatedEmployees);
         } catch (error) {
             console.error('Error deleting employee:', error);
         }
     };
 
     const renderEmployeeItem = ({ item }) => (
-        <TouchableOpacity
-            style={styles.employeeItemContainer}
-            onPress={() => navigation.navigate('EmployeeDetails', { id: item.id })}
-            onLongPress={() => handleDeleteEmployee(item.id)}
-        >
-            <Text style={styles.employeeName}>{item.employee_name}</Text>
-            <Text style={styles.employeeDetails}>Salary: {item.employee_salary}</Text>
-            <Text style={styles.employeeDetails}>Age: {item.employee_age}</Text>
-        </TouchableOpacity>
+        <View style={styles.employeeItemContainer}>
+            <TouchableOpacity
+                style={styles.employeeInfoContainer}
+                onPress={() => navigation.navigate('EmployeeDetails', { id: item.id })}
+            >
+                <Text style={styles.employeeName}>{item.employee_name}</Text>
+                <Text style={styles.employeeDetails}>Salary: {item.employee_salary}</Text>
+                <Text style={styles.employeeDetails}>Age: {item.employee_age}</Text>
+            </TouchableOpacity>
+            <View style={styles.actionsContainer}>
+                <TouchableOpacity
+                    style={[styles.actionButton, styles.editButton]}
+                    onPress={() => navigation.navigate('EmployeeForm', { id: item.id })}
+                >
+                    <Text style={styles.actionButtonText}>Edit</Text>
+                </TouchableOpacity>
+                <TouchableOpacity
+                    style={[styles.actionButton, styles.deleteButton]}
+                    onPress={() => handleDelete(item.id)}
+                >
+                    <Text style={styles.actionButtonText}>Delete</Text>
+                </TouchableOpacity>
+            </View>
+        </View>
     );
 
     return (
         <View style={styles.container}>
+            <View style={styles.searchContainer}>
+                <FontAwesome name="search" size={20} color="#007bff" style={styles.searchIcon} />
+                <TextInput
+                    style={styles.searchInput}
+                    value={searchQuery}
+                    onChangeText={setSearchQuery}
+                    placeholder="Search employee"
+                    placeholderTextColor="#007bff"
+                />
+            </View>
             <FlatList
-                data={employees}
+                data={searchResults}
                 keyExtractor={(item) => item.id.toString()}
                 renderItem={renderEmployeeItem}
             />
@@ -63,19 +104,67 @@ const styles = StyleSheet.create({
         paddingHorizontal: 16,
         paddingTop: 16,
     },
+    searchContainer: {
+        flexDirection: 'row',
+        alignItems: 'center',
+        marginBottom: 16,
+        height: 40,
+        backgroundColor: '#fff',
+        borderColor: '#007bff',
+        borderWidth: 1,
+        borderRadius: 4,
+        paddingHorizontal: 8,
+    },
+    searchIcon: {
+        marginRight: 10,
+    },
+    searchInput: {
+        flex: 1,
+        color: '#007bff',
+    },
     employeeItemContainer: {
         marginBottom: 16,
         padding: 16,
-        backgroundColor: '#e8e8e8',
+        backgroundColor: '#fff',
+        borderColor: '#e8e8e8',
         borderRadius: 8,
+        flexDirection: 'row',
+        justifyContent: 'space-between',
+        alignItems: 'center',
+    },
+    employeeInfoContainer: {
+        flex: 1,
+        marginRight: 16,
     },
     employeeName: {
         fontSize: 18,
         fontWeight: 'bold',
+        marginBottom: 8,
     },
     employeeDetails: {
         fontSize: 16,
-        marginTop: 8,
+    },
+    actionsContainer: {
+        flexDirection: 'row',
+    },
+    actionButton: {
+        paddingHorizontal: 12,
+        paddingVertical: 6,
+        marginLeft: 8,
+        borderRadius: 4,
+        alignItems: 'center',
+        justifyContent: 'center',
+    },
+    editButton: {
+        backgroundColor: '#007bff',
+    },
+    deleteButton: {
+        backgroundColor: '#dc3545',
+    },
+    actionButtonText: {
+        color: '#fff',
+        fontSize: 14,
+        fontWeight: 'bold',
     },
     addButton: {
         marginTop: 16,
