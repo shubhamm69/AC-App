acad3c25da946672ef4ae8e5e25b5e3078e28dc2
Shubham Singh <shubham212a@gmail.com>
2023-07-15 21:38:54 +0530

    fixed in app folder structure

---
+++ Assignment2/client/components/EmployeeForm.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+function EmployeeForm() {
+    return (
+        <div>
+
+        </div>
+    )
+}
+
+export default EmployeeForm
---
+++ Assignment2/client/screens/EmployeeListScreen.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+function EmployeeListScreen() {
+    return (
+        <div>
+
+        </div>
+    )
+}
+
+export default EmployeeListScreen
